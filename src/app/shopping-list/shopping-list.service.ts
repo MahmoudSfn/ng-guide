79d2a56387595ce414a745dfa052ad90755afde6
Mahmoud Souayfan <mahmoudsouayfan@gmail.com>
2022-10-17 15:31:00 +0200

    applying forms

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,11 @@
 import { EventEmitter } from "@angular/core";
 import { Ingredient } from "src/shared/ingredient.model";
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
+
     ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Banana', 3),
@@ -12,13 +15,21 @@ export class ShoppingListService {
         return this.ingredients.slice();
     }
 
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+
     }
 }
