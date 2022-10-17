950d1db836605e8187772bf545c6c4c98a3c463d
Mahmoud Souayfan <mahmoudsouayfan@gmail.com>
2022-10-17 15:38:44 +0200

    fix: resetting form + cleanning

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -30,6 +30,7 @@ export class ShoppingListService {
     }
 
     updateIngredient(index: number, newIngredient: Ingredient) {
-
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
