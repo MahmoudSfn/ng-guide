7e637e7463d595c475ade85306b1d1f0accf00cf
Mahmoud Souayfan <mahmoudsouayfan@gmail.com>
2022-10-18 10:19:46 +0200

    add ingredients + form validators

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -35,6 +35,33 @@ export class RecipeService {
                 new Ingredient('Cooked smoked sausage', 1),
                 new Ingredient('Yellow onion', 1)
             ]
+        ),
+        new Recipe(
+            'Shrimp Ceviche',
+            'The EASIEST way to make traditional shrimp ceviche and it tastes the BEST!! Ready in 30 minutes, great as an appetizer, side, or as a healthy main course that everyone LOVES!!',
+            'https://www.averiecooks.com/wp-content/uploads/2019/07/ceviche-6.jpg',
+            [
+                new Ingredient('Shrimp', 1),
+                new Ingredient('Lime juice', 1),
+                new Ingredient('Tomatoes', 1),
+                new Ingredient('Avocado', 1),
+                new Ingredient('Jalapeño or serrano chile pepper', 1),
+                new Ingredient('Cucumber', 1),
+                new Ingredient('Red onion', 1),
+            ]
+        ),
+        new Recipe(
+            'Grilled Lime Cilantro Chicken',
+            'EASY, ready in 10 minutes, and the cilantro chicken is so TENDER, juicy, and full of Mexican-inspired flavor!! Put this HEALTHY chicken recipe on your summer menu rotation and it’ll be a hit with everyone!!',
+            'https://www.averiecooks.com/wp-content/uploads/2019/06/limecilantrochicken-8.jpg',
+            [
+                new Ingredient('Boneless skinless chicken breasts', 1),
+                new Ingredient('Lime juice and zest', 1),
+                new Ingredient('Olive oil', 1),
+                new Ingredient('Honey', 1),
+                new Ingredient('Fresh cilantro', 1),
+                new Ingredient('Kosher salt and pepper', 1),
+            ]
         )
     ];
 
